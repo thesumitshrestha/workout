a0aa0ffd89fcc12d9d4154a24539799c5b610c3d
Sumit Shrestha <summitshrestha8@gmail.com>
2023-10-03 22:35:26 -0500

    Backend Completed

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,30 +1,29 @@
 const express = require('express');
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+  deleteWorkout,
+  updateWorkout,
+} = require('../controllers/workoutController');
+
+const Workout = require('../models/workoutmodel');
 
 const router = express.Router();
 
 // Get all workouts
-router.get('/', (req, res) => {
-  res.json({ msg: 'Get all workouts' });
-});
+router.get('/', getWorkouts);
 
 // Workout detail
-router.get('/:id', (req, res) => {
-  res.json({ msg: 'Workout Detail' });
-});
+router.get('/:id', getWorkout);
 
 // Post a workout
-router.post('/', (req, res) => {
-  res.json({ msg: 'POST a new workout' });
-});
+router.post('/', createWorkout);
 
 // Delete a workout
-router.delete('/:id', (req, res) => {
-  res.json({ msg: 'DELETE a new workout' });
-});
+router.delete('/:id', deleteWorkout);
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-  res.json({ msg: 'UPDATE a new workout' });
-});
+router.patch('/:id', updateWorkout);
 
 module.exports = router;
